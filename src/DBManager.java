00f00c72924b563c193d6a50883e36f0d383d52d
tinayi08 <christinayi827@gmail.com>
2021-05-09 17:04:07 -0700

    added java docs

--- src/DBManager.java
+++ src/DBManager.java
@@ -16,19 +16,29 @@ public class DBManager {
 
         return returnPerson;
     }
+    /*
 
-    public void searchIndividual() {
-
+    This method asks the user to enter a first name and a last name.
+    The name will be added to a Person object and method will return a person.
+     */
+    public Person searchIndividual() {
+        //Add searching for last name too
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter the first name of the individual you are searching for:");
-        String search = scan.next();
-        for (Person p : this.data) {
-           if(search.equalsIgnoreCase(p.getfName())) {
-               System.out.println(p);
-           }
-        }
-    }
+        String searchFName = scan.next();
+        System.out.println("Please enter the last name of the individual you are searching for:");
+        String searchLName = scan.next();
+        Person searchPerson = new Person (searchFName, searchLName);
 
+        return searchPerson;
+    }
+    /*
+
+    This method sets up an initial bach of people to have in the ArrayList of data.
+    No vaccine information has been added yet.
+
+    Need to later change this into SQL
+     */
     public void setUpDataTesting() {
         //add people into the array to have some people in there
         //practice purposes only - will use text file or DB later on
@@ -40,21 +50,37 @@ public class DBManager {
         Person five = new Person("Amy", "Lee", 1, 22, 1965);
         Person six = new Person("Jordan", "Clarke", 11, 27, 1990);
         Person seven = new Person("Kameron"," Clarke", 4, 2, 1991);
+        Person eight = new Person("Amy", "Lee", 1, 22, 1965);
 
-        this.data.add(one);
+        //change methods
+        addPersonEntry(one);
         this.data.add(two);
         this.data.add(three);
         this.data.add(four);
         this.data.add(five);
         this.data.add(six);
         this.data.add(seven);
+        this.data.add(eight);
 
     }
 
+    /*
+
+    This method takes in a Person object.
+    It will add a person into the ArrayList of data.
+     */
     public void addPersonEntry(Person person) {
+        //does this person exist already? - do a check
+        //if exists, are you changing profile or adding another person
+        //updating profile should be different method
         data.add(person);
     }
 
+    /*
+
+    This method will iterate through the ArrayList of data.
+    It will display all elements of the ArrayList
+     */
     public void printDataArray() {
         System.out.println("Entries:");
         for (Person person : data) {
