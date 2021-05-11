bd521a187f645fdb9efe2e28fcebcc6283c1fcfd
tinayi08 <christinayi827@gmail.com>
2021-05-10 23:02:13 -0700

    added a method that checks to see if the individual is already in the ArrayList

--- src/Validator.java
+++ src/Validator.java
@@ -6,11 +6,27 @@ public class Validator {
     public Validator() {
 
     }
+    /*
 
+    This method checks the ArrayList to see if the newly created Person object
+    is a duplicate
+     */
     public boolean duplicateEntry(Person person, ArrayList<Person> data) {
-        //if entry already exists return true
+        String fName = person.getfName();
+        String lName = person.getlName();
+        int month = person.getDob().month;
+        int day = person.getDob().day;
+        int year = person.getDob().year;
+
+        for (Person p : data) {
+            if(fName.equalsIgnoreCase(p.getfName()) && lName.equalsIgnoreCase(p.getlName()) && month == p.getDob().month &&
+            day == p.getDob().day && year == p.getDob().year) {
+                return true;
+            }
+        }
 
         return false;
+
     }
 
     public int startDataInputValidEntry(int navigate) {
