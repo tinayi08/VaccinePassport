060560cda0bf70e8c080384a17f8fee69f48e922
tinayi08 <christinayi827@gmail.com>
2021-06-14 23:34:49 -0700

    changed access modifiers to private

--- src/TestDisplayPerson.java
+++ src/TestDisplayPerson.java
@@ -5,20 +5,20 @@ public class TestDisplayPerson {
 
     Scanner scan = new Scanner(System.in);
 
-    public int searchBirth(String search) {
+    private int searchBirth(String search) {
         System.out.println("Please enter the birth " + search + ":");
         int birth = scan.nextInt();
         return birth;
     }
 
-    public String searchName(String search) {
+    private String searchName(String search) {
 
         System.out.println("Please enter the " + search + " name:");
         String name = scan.next();
         return name;
     }
 
-    public int searchOption() {
+    private int searchOption() {
 
         System.out.println("\nPlease select from the following options:");
         System.out.println("1. Enter First/Last Name only");
